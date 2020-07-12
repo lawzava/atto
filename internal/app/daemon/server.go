fd2a6bcddac2da666d98c053de4b3e3be329e5e8
z <z@v0.vc>
2020-07-12 19:55:52 +0200

    :art: explicit mux

--- internal/app/daemon/server.go
+++ internal/app/daemon/server.go
@@ -8,10 +8,11 @@ import (
 const addr = ":9182"
 
 func (d *Daemon) Start() error {
-	http.HandleFunc("/", handleSites(d.sites))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleSites(d.sites))
 
 	d.Logger.Infof("Server is running on %s port", addr)
 
 	// Serve TLS here as well
-	return http.ListenAndServe(addr, nil)
+	return http.ListenAndServe(addr, mux)
 }
