48a80477587d1f8681f45f3fa121b982e87041ed
z <z@v0.vc>
2020-07-12 19:04:07 +0200

    :sparkles: handle multi-domain sites

--- internal/app/daemon/server.go
+++ internal/app/daemon/server.go
@@ -3,9 +3,6 @@ package daemon
 import (
 	"fmt"
 	"net/http"
-	"path"
-
-	"github.com/gin-gonic/gin"
 )
 
 const (
@@ -16,46 +13,9 @@ const (
 func (d *Daemon) Start() error {
 	e := d.server
 
-	for _, site := range d.sites {
-		var (
-			relativePath = "/" + site.domain
-			absolutePath = joinPaths(e.BasePath(), relativePath)
-			fs           = gin.Dir(site.directoryPath, false)
-		)
-
-		e.Handle(
-			http.MethodGet,
-			path.Join(relativePath, "/*filepath"),
-			handleStaticFiles(absolutePath, fs),
-		)
-	}
-
-	e.Handle(http.MethodGet, "/status", statusHandler)
+	e.Handle(http.MethodGet, "/*filepath", handleStaticFiles("/", d.sites))
 
 	d.Logger.Infof("Server is running on %d port", port)
 
 	return d.server.Run(fmt.Sprintf(":%d", port))
-}
-
-func joinPaths(basePath, relativePath string) string {
-	if relativePath == "" {
-		return basePath
-	}
-
-	finalPath := path.Join(basePath, relativePath)
-	appendSlash := lastChar(relativePath) == '/' && lastChar(finalPath) != '/'
-
-	if appendSlash {
-		return finalPath + "/"
-	}
-
-	return finalPath
-}
-
-func lastChar(str string) uint8 {
-	if str == "" {
-		panic("The length of the string can't be 0")
-	}
-
-	return str[len(str)-1]
 }
--- internal/app/daemon/staticfiles.go
+++ internal/app/daemon/staticfiles.go
@@ -2,25 +2,49 @@ package daemon
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func handleStaticFiles(absolutePath string, fs http.FileSystem) gin.HandlerFunc {
-	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
-
+func handleStaticFiles(absolutePath string, sites []site) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		file := c.Param("filepath")
+		for _, site := range sites {
+			if getDomainName(c.Request.Host) != site.domain {
+				continue
+			}
 
-		// Check if file exists and/or if we have permission to access it
-		f, err := fs.Open(file)
-		if err != nil {
-			c.Writer.WriteHeader(http.StatusNotFound)
+			fs := gin.Dir(site.directoryPath, false)
+			fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+
+			file := c.Param("filepath")
+
+			// Check if file exists and/or if we have permission to access it
+			f, err := fs.Open(file)
+			if err != nil {
+				renderNotFound(c, "file not found")
+				return
+			}
+
+			_ = f.Close()
+
+			fileServer.ServeHTTP(c.Writer, c.Request)
+
 			return
 		}
 
-		_ = f.Close()
-
-		fileServer.ServeHTTP(c.Writer, c.Request)
+		renderNotFound(c, "domain not found")
 	}
+}
+
+func renderNotFound(c *gin.Context, message string) {
+	c.JSON(http.StatusNotFound, gin.H{
+		"code":    http.StatusNotFound,
+		"status":  http.StatusText(http.StatusNotFound),
+		"message": message,
+	})
+}
+
+func getDomainName(host string) string {
+	return strings.Split(host, ":")[0]
 }
